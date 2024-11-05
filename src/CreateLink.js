0b4eb52351590b00a1f2dd61b064f148402be83b
boris <boris@gratchev.com>
2024-11-05 11:28:47 +0100

    send date and time as string

--- src/CreateLink.js
+++ src/CreateLink.js
@@ -4,12 +4,16 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import axios from 'axios';
+import { useState } from 'react';
 
 function CreateLink() {
+    const [pickedDateTime, setPickedDateTime] = useState(null)
+
     const sendCreate = async (guess) => {
         try {
-
-            const response = await axios.post('http://localhost:8080/create');
+            let time = pickedDateTime.format('HH:mm')
+            let date = pickedDateTime.format('DD.MM.YYYY')
+            const response = await axios.post('http://localhost:8080/create', {date: date, time: time});
             console.log(response.data)
         } catch (error) {
             console.error('Error sending guess:', error);
@@ -20,7 +24,7 @@ function CreateLink() {
         <LocalizationProvider dateAdapter={AdapterDayjs}>
             <h1>Create Link</h1>
             <DatePicker label="coin toss reveal date" />
-            <DateTimePicker label="Basic date time picker" />
+            <DateTimePicker label="Basic date time picker" value={pickedDateTime} onChange={(newValue)  => setPickedDateTime(newValue)}/>
             <Button variant="contained" disableElevation onClick={sendCreate}>Create Link</Button>
         </LocalizationProvider>
     );
