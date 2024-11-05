6d94cb0176fec9c75a639f25444af38302531431
boris <boris@gratchev.com>
2024-11-05 10:43:35 +0100

    backend requests

--- src/CreateLink.js
+++ src/CreateLink.js
@@ -3,18 +3,27 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
-
-
+import axios from 'axios';
 
 function CreateLink() {
-  return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <h1>Create Link</h1>
-      <DatePicker label="coin toss reveal date"/>
-      <DateTimePicker label="Basic date time picker" />
-      <Button variant="contained" disableElevation>Create Link</Button>
-    </LocalizationProvider>
-  );
+    const sendCreate = async (guess) => {
+        try {
+
+            const response = await axios.post('http://localhost:8080/create');
+            console.log(response.data)
+        } catch (error) {
+            console.error('Error sending guess:', error);
+        }
+    };
+
+    return (
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <h1>Create Link</h1>
+            <DatePicker label="coin toss reveal date" />
+            <DateTimePicker label="Basic date time picker" />
+            <Button variant="contained" disableElevation onClick={sendCreate}>Create Link</Button>
+        </LocalizationProvider>
+    );
 }
 
 export default CreateLink
